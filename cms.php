6d1e3d1da59448e9e7a2ae23c9464e66330d9b4d
Sunil Shrestha <yesubc@gmail.com>
2016-01-22 01:56:00 +0545

    Static pages added.

---
+++ cms.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content='width=device-width, initial-scale=1.0' name='viewport'>
+    <title>CMS Page : TMT Maldives</title>
+    <?php
+        include("includes/stylesheet.php");
+        include("includes/favico.php");
+    ?>
+    <!--[if lte IE 7]><style>.main{display:none;} .support-note .note-ie{display:block;}</style><![endif]-->
+    <!--[if lt IE 9]>
+    <script scr="assets/js/html5shiv/html5shiv.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <div class="main-container">
+        <header>
+            <?php
+                include("includes/header.php");
+                include("includes/primary-menu.php");
+            ?>
+        </header>
+        <div class="col2-right-layout page">
+            <div class="breadcrumb">
+                <ul class="container">
+                    <li><a href="#">Home</a> <span class="divider">/</span></li>
+                    <li><a href="#">Company</a> <span class="divider">/</span></li>
+                    <li class="active">About Us</li>
+                </ul>
+            </div>
+            <div class="container">
+                <div class="main">
+                    <div class="col-main">
+                        <h2>CMS Page Header</h2>
+                            <div class="entry-page">
+                                <p>Abode Paradise Limited is a registered company in Republic of Maldives and is a tour operator registered and licensed from the Ministry of Tourism Arts and Culture. The company's prime business is tourism and travel trade, where it sells Maldives Tourism products namely resort islands, its room, live boards, excursions and many other travel related services to both travel agents and end customers.</p>
+                                <p>Our services have, since we commenced operations, sustained an enviable reputation from both long-term international partners, as well as with returning clients.</p>
+                                <p>With almost 5 years of providing service excellence within the Maldivian tourism industry, Abode Paradise maintains its leading position through an experienced, energetic and competent staff, directly supervised by the directors of the company.</p>
+                                <p>We have, in particular, focused on developing existing, as well as new markets, to offer the Maldives as a unique, first-rate destination. Our partners and clients include travel agents, tour operators and individual travelers. Each also represents an important part of our multilayered distribution network.</p>
+                                <p>Abode Paradise offers uniquely low wholesale rates for our partners. Allowing them to access the Maldivian market and enable them to provide their clients and partners with very attractive accommodation rates.</p>
+                                <p>It's also a pleasure for us to be a long serving member of the Maldives Tourism Promotion Board, which represents the Maldives at all major travel and trade fairs around the World.</p>
+                                <p>Abode Paradise is the preferred choice for thousands of visitors booking hotel and resort holidays on the Maldives. They choose us because we can help them have an outstanding holiday at an exceptionally good value.</p>
+                                <p>Abode Paradise can offer you such a unique range of quality accommodations at such affordable rates. We can provide these prices, as we are located on the Maldives and having long-term agreements and contracts with local hotels, resorts and tour operators.</p>
+
+                                
+                        </div>
+                    </div>
+                </div>
+                <div class="col-right">
+                    <div class="widgets">
+                        <h3 class="widgets-title hide">Pages</h3>
+                        <ul class="list-group">
+                            <li><a href="#" title="Our Company">Our Company</a></li>
+                             <li><a href="#" title="Our Vision">Our Vision</a></li>
+                             <li><a href="#" title="Our Mission">Our Mission</a></li>
+                             <li><a href="#" title="Our Objective">Our Objective</a></li>
+                       </ul>
+                   </div>
+                   
+                   <div class="widgets">
+                       <h3 class="widgets-title">Maldives Popular Resorts</h3>
+                   </div>
+
+                   <div class="widgets">
+                    <img src="assets/images/adverts/good-mordern.png" alt="" class="roundedborder">
+                   </div>
+                </div>  
+            </div>  
+        </div>
+        <?php include("includes/footer.php");?>
+    </div>
+<script src="assets/js/jquery/jquery-1.9.1.min.js"></script>
+<script src="assets/js/modernizr/modernizr.custom.70736.js"></script>
+<script src="assets/js/ddmenu/ddmenu.js"></script>
+<script src="assets/js/owl-carousel/owl.carousel.js"></script>
+<!--Gama Gallery-->
+<script src="assets/js/gamma/jquery.masonry.min.js"></script>
+<script src="assets/js/gamma/gamma.js"></script>
+<script src="assets/js/main.js"></script>
+</body>
+</html>
